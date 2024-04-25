f4fd44b7546b1933b998f199364c615da301f1fe
Aymane Achfennar <aachfenn@c1r8p2.1337.ma>
2024-04-25 11:51:49 +0100

    .

--- Untitled/srcs/requirements/mariadb/tools/script.sh
+++ Untitled/srcs/requirements/mariadb/tools/script.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-sleep 10
-# while ! mysqladmin ping --silent; do
-    # sleep 1
-# service mysql start;
+# sleep 10
+# this line loads the variables in the .env file
+source /home/ayac/Desktop/ft_/srcs/.env
 mysqld &
+while ! mysqladmin ping --silent; do
+    sleep 1
+done
+echo "MariaDB is running correctly."
+
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
