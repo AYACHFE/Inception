d3dbf55f6b5c3dc8f83157a26e8273fc2950ecb3
AYACHFE <aymaneachfennar@gmail.com>
2024-05-10 12:50:26 +0100

    docker compose running but word press has problems

--- Untitled/srcs/requirements/wordpress/tools/script.sh
+++ Untitled/srcs/requirements/wordpress/tools/script.sh
@@ -2,10 +2,18 @@
 # sleep 5
 . ./.env
 
-wp config create	--allow-root \
-			--dbname=$SQL_DATABASE \
-			--dbuser=$SQL_USER \
-			--dbpass=$SQL_PASSWORD \
-			--dbhost=mariadb:3306 --path='/var/www/wordpress'
-
-cat /var/www/wordpress/wp-config.php
+echo "-----------------_________------------------"
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
+    echo "wp-config.php does not exist, creating new file"
+    wp config create --allow-root \
+        --dbname=$SQL_DATABASE \
+        --dbuser=$SQL_USER \
+        --dbpass=$SQL_PASSWORD \
+        --dbhost=mariadb:3306 --path='/var/www/wordpress'
+else
+    echo "wp-config.php exists, updating existing file"
+    wp config set DB_NAME $SQL_DATABASE --allow-root --path='/var/www/wordpress'
+    wp config set DB_USER $SQL_USER --allow-root --path='/var/www/wordpress'
+    wp config set DB_PASSWORD $SQL_PASSWORD --allow-root --path='/var/www/wordpress'
+    wp config set DB_HOST mariadb:3306 --allow-root --path='/var/www/wordpress'
+fi
