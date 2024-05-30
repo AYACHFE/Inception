27fb5230400fe3e39297a083345da07c6cf17f9c
AYACHFE <aymaneachfennar@gmail.com>
2024-05-30 07:33:05 -0400

    sh is main process fixed on mariadb and wordpress

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,18 +1,16 @@
-# #!/bin/bash
+#!/bin/bash
 
 echo "script in\n";
 
 mariadb-install-db  --user=mysql --datadir=/var/lib/mysql
 
 cat << EOF > /tmp/init_db.sql
-
-
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
-
 EOF
 mariadbd --user=mysql --bootstrap < /tmp/init_db.sql
+echo "script out\n";
 exec mariadbd --user=mysql --bind-address=0.0.0.0
