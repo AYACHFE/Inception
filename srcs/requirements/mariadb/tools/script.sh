57c008dc94133a5e99fa3e1d1080a455122c2351
AYACHFE <aymaneachfennar@gmail.com>
2024-05-30 09:17:10 -0400

    minimal

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-echo "script in\n";
-
 mariadb-install-db  --user=mysql --datadir=/var/lib/mysql
 
-cat << EOF > /tmp/init_db.sql
+cat << EOF > /tmp/db_config.sql
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%';
+GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-mariadbd --user=mysql --bootstrap < /tmp/init_db.sql
-echo "script out\n";
+mariadbd --user=mysql --bootstrap < /tmp/db_config.sql
 exec mariadbd --user=mysql --bind-address=0.0.0.0
